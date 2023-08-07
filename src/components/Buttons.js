500787396348ef2b374e3e0ef7944822003d9b6b
Natasha <nhodg2@gmail.com>
2023-08-06 21:31:40 -0500

    Update Button.js component

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,10 +1,18 @@
-const Buttons = () => {
+import satData from "./satData";
+
+const Buttons = (props) => {
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-      </div>
+      {props.displaySats.map((sat, id) => {
+        return (
+          <button onClick={() => props.filterByType(sat)} key={id}>
+            {sat} Orbit
+          </button>
+        );
+      })}
+      <button onClick={() => props.setSat(satData)}>All orbits</button>
+    </div>
   );
-};
+}
 
 export default Buttons;
