0a40dd3f7d095c09c5e9afe9912669e748e27dc9
Natasha <nhodg2@gmail.com>
2023-08-11 16:24:13 -0500

    Update Table component to display the sat information for each orbit level

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,15 +1,29 @@
 const Table = ({ sat }) => {
+  const satInfo = sat.map((data, id) => {
+    //when I had map(id, data) it did not work at all...why?
+    //because the id is included in the data?
+    return (
+      <tr key={id} >
+        <td>{data.name}</td>
+        <td>{data.type}</td>
+        <td>{data.launchDate}</td>
+        <td>{data.operational ? "Active" : "Inactive"}</td>
+      </tr>
+    );
+  });
+
   return (
       <table>
        <thead>
         <tr>
-          <th>Header TBD</th>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
         </tr>
         </thead>
         <tbody>
-        <tr>
-          <td>Row Data TBD</td>
-        </tr>
+          {satInfo}
         </tbody>
       </table>
   );
